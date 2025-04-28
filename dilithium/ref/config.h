decfdf5533c6efc1e4cb487e79150338ddf63955
Hunter Trujillo <cryptoquick@pm.me>
2025-04-28 06:48:01 -0600

    Makefile improvements and project cleanup.

--- dilithium/ref/config.h
+++ dilithium/ref/config.h
@@ -11,15 +11,15 @@
 #endif
 
 #if DILITHIUM_MODE == 2
-#define CRYPTO_ALGNAME "Dilithium2"
+#define CRYPTO_ALGNAME_DILITHIUM "Dilithium2"
 #define DILITHIUM_NAMESPACETOP pqcrystals_dilithium2_ref
 #define DILITHIUM_NAMESPACE(s) pqcrystals_dilithium2_ref_##s
 #elif DILITHIUM_MODE == 3
-#define CRYPTO_ALGNAME "Dilithium3"
+#define CRYPTO_ALGNAME_DILITHIUM "Dilithium3"
 #define DILITHIUM_NAMESPACETOP pqcrystals_dilithium3_ref
 #define DILITHIUM_NAMESPACE(s) pqcrystals_dilithium3_ref_##s
 #elif DILITHIUM_MODE == 5
-#define CRYPTO_ALGNAME "Dilithium5"
+#define CRYPTO_ALGNAME_DILITHIUM "Dilithium5"
 #define DILITHIUM_NAMESPACETOP pqcrystals_dilithium5_ref
 #define DILITHIUM_NAMESPACE(s) pqcrystals_dilithium5_ref_##s
 #endif
